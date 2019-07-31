cb8697b4f6ac19285a87b6a5ed3f11e36c17f47b
lenon777 <amytsak90@gmail.com>
2019-07-31 19:39:17 +0300

    taskTwo

---
+++ taskTwo.js
@@ -0,0 +1,21 @@
+function Animal(breed) {
+    this.breed = breed;
+}
+Animal.prototype.getBreed = function () {
+    console.log("Wof-wof I am " + this.breed)
+}
+function Dog(breed) {
+    Animal.call(this,breed);
+    if (!(this instanceof Dog)){
+        return new Dog();
+   }
+}
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+const haski = new Dog('haski');
+const pitBul = Dog('pitBull');
+haski.getBreed(); // haski
+// still works
+pitBul.getBreed(); // pitBull
+pitBul instanceof Dog // true
