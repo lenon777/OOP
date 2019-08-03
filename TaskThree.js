75704acc27c1b1760ca87bec149ce4db4385a41a
lenon777 <amytsak90@gmail.com>
2019-08-03 17:37:23 +0300

    third task

--- TaskThree.js
+++ TaskThree.js
@@ -1,29 +1,32 @@
 const obj1 = {
-    sayHi: () => console.log('Hi!')
-   };
-   
-   const obj2 = {
-    showNumber: () => console.log(parseFloat(Math.random() * 100))
-   };
-   
-   const obj3 = {
-    showRandomSymbol: () => console.log(String.fromCharCode(parseFloat(Math.random() * 100)))
-   };
-   function makeChain(){
-       console.log(arguments)
-       for(var key in arguments) {
-        console.log(arguments[key])
-        //makeChain.prototype = Object.create(arguments[key].prototype);
-       }
-    //    if (!(this instanceof makeChain)){
-    //     return new makeChain();
-   //}
-   }
+  sayHi: () => console.log('Hi!')
+};
 
-   const bigFatObject = makeChain(obj1, obj2, obj3);
-   
-   console.log('​bigFatObject', bigFatObject);
-   bigFatObject.sayHi();
-   bigFatObject.showNumber();
-   bigFatObject.showRandomSymbol();
-   
+const obj2 = {
+  showNumber: () => console.log(parseFloat(Math.random() * 100))
+};
+
+const obj3 = {
+  showRandomSymbol: () => console.log(String.fromCharCode(parseFloat(Math.random() * 100)))
+};
+
+function MakeChain() {
+  var arrOfArguments = [];
+  for (var key in arguments) {
+    arrOfArguments.push(arguments[key]);
+  }
+  for (var i = arrOfArguments.length - 1; i > 0; i--) {
+    arrOfArguments[i].__proto__ = arrOfArguments[i - 1];
+
+  }
+  var inst = {};
+  inst.__proto__ = arrOfArguments[arrOfArguments.length - 1];
+  return inst;
+}
+
+const bigFatObject = MakeChain(obj1, obj2, obj3);
+
+console.log('​bigFatObject', bigFatObject);
+bigFatObject.sayHi();
+bigFatObject.showNumber();
+bigFatObject.showRandomSymbol();
